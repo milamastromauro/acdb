b11feb23451339bf53e02cbc1f9cdd69f7f86258
Milamastromauro <41583379+Milamastromauro@users.noreply.github.com>
2018-11-29 05:37:29 -0200

    alterada pagina de produto

--- resources/views/produto.blade.php
+++ resources/views/produto.blade.php
@@ -14,39 +14,51 @@
 	@include('header')
 
 		<section>
-        <div class="container-fluid" id="present">
-					<img src="{{ asset('/imagens/product-template.png') }}" id="modelo" alt="product-image">
-        <div class="container-fluid" id="about">
-        <div class="container-fluid" id="descritivo">
-            <h1 id="name-produto">Product Name</h1>
-            <h4>R$ X</h4>
-            <p id="description-produto"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id blandit metus. Curabitur eu fermentum augue, vitae iaculis est. Sed posuere posuere placerat. Phasellus faucibus lorem ipsum, quis ullamcorper risus hendrerit eget. Nunc pellentesque consequat nibh pellentesque vulputate. Cras convallis porttitor velit at aliquet. Maecenas vel auctor mi.
-            </p>
-            <form>
-            <span>
-                <select class="size">
+			<div class="container">
+				<div class="row justify-content-around ">
+								<div class="col-3 col-xs-3 col-sm-3 col-md-6 col-lg-6 col-xl-6">
+			      <img src="{{ asset('/imagens/produtos-categoria/vestido_geometrico_lado.jpg') }}" class="border_produto" id="modelo" alt="product-image" height="415" width="288" >
+			    </div>
+
+			    <div class="col-3 col-xs-3 col-sm-3 col-md-6 col-lg-6 col-xl-6 blocoproduto">
+			       <h1>Vestido Geométrico</h1>
+					 </br>
+						  <h5> R$ 320</h5>
+						</br>
+							<div>
+							<span class="titulo_produto"> Descrição </span>
+						</br>
+							<span></br>Vestido Geométrico Branco/Vermelho/Amarelo/Preto, possui estampa em formas geométrica e sobreposição em tela. Mangas com ombro à mostra.
+							Confeccionado em 48% Acrílico, 48% Viscose e 4% Elastano. <span>
+							</div></br>
+								<div class="row justify-content-around" id="descritivo">
+
+          	<span class="position_name_d"> Tamanho </span>
+                <select class="size col-4 col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
                     <option name= "tamanho" value="PP">PP</option>
                     <option name= "tamanho" value="P">P</option>
                     <option name= "tamanho" value="M">M</option>
                     <option name= "tamanho" value="G">G</option>
                     <option name= "tamanho" value="GG">GG</option>
                 </select>
-            </span>
-            <span>
-                <select class="color">
+
+					<span class="position_name_d"> Cor </span>
+                <select class="color col-4 col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
                     <option name="cor" value="color-1">Cor 1</option>
                     <option name="cor" value="color-2">Cor 2</option>
                     <option name="cor" value="color-3">Cor 3</option>
                 </select>
             </span>
-            <span>
-                <button class="conclude" type="submit"><input type="hidden" name="id0001">COMPRAR!</button>
-            </span>
-            </form>
-            </div>
-            </div>
-    </div>
-		</section>
+							 <button class="conclude" type="submit">COMPRAR</button>
+			    </div>
+			  </div>
+
+			</div>
+		</div>
+	</section>
+
+
+
  @include('footer')
 		<!-- SCRIPTS DE JS PARA O BOOTSTRAP -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
