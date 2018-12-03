64b000dfbf0d815e1f02ff21d867f0b58c9e2740
Milamastromauro <41583379+Milamastromauro@users.noreply.github.com>
2018-12-03 15:09:39 -0200

    alterada pag contato - incluido maps

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -7,6 +7,7 @@
 		<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Ubuntu|Poppins" rel="stylesheet">
 		<link href="{{ asset('/css/novo.css') }}" rel="stylesheet">
 		<title>A Cabeça do Buda - Camisetas</title>
 	</head>
@@ -20,16 +21,20 @@
       <div class="row">
         <div class="col-lg-7">
           <img class="imgcontato" src="{{ asset('/imagens/contato.jpg') }}" alt="">
-        </div><!-- fim col-lg-08-->
+        </div>
         <div class="col-lg-5 margintopdez">
-          <h1 class="topico"> Nossas Lojas - Guide Shop </h1>
-          <br>
-          <br>
-          <br>
-          <p class="para">Você pode retirar suas compras em qualquer um dos nossos Guide Shops. Os Pedidos chegam em até dois dias úteis e com frete grátis. Para escolher essa opção, é só marcar "Retirar no Guide Shop" na página de pagamento e selecionar o Guide Shop mais conveniente pra você.</p>
-          <br>
-          <br><b class="tituloinfos">ENDEREÇO: <i class="cardoso">Cardoso de  Melo,90</i></b>
-          <br><b class="tituloinfos">TELEFONE: <i class="cardoso">+55 11 3333-3333</i></b>
+					<div class="topico">
+          <h1> Nossas Lojas - Guide Shop </h1>
+				</div>
+					<div class="para">
+          <span>Você pode retirar suas compras em qualquer um dos nossos Guide Shops. </br> Os Pedidos chegam em até dois dias úteis e com frete grátis. Para escolher essa opção, é só marcar "Retirar no Guide Shop" na página de pagamento e selecionar o Guide Shop mais conveniente pra você.</span>
+				</div>
+					<div class="infos_loja_contato">
+          <span class="tituloinfos">ENDEREÇO:<i class="cardoso">Avenida Dr. Cardoso de Melo,90</i></span>
+          <span class="tituloinfos">TELEFONE:<i class="cardoso">+55 11 3333-3333</i></span>
+					</div>
+					<div class="mapouter"><div class="gmap_canvas"><iframe width="460" height="320" id="gmap_canvas" src="https://maps.google.com/maps?q=dr%20cardoso%20de%20melo%2C%2090%20-%20Brazil%20-%20S%C3%A3o%20Paulo%2C%20SP&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.pureblack.de/webdesign-muenster/"></a></div><style>.mapouter{text-align:right;height:297px;width:499px;}.gmap_canvas {overflow:hidden;background:none!important;height:297px;width:499px;}</style></div>
+				</div>
         </div>
 
 
