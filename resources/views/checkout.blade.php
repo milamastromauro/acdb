513b2612bfbde9bcb6bf856212a1362d1af6ede1
Dinara Lima <dinara.lima@gmail.com>
2018-12-04 22:23:30 -0200

    aplicacao do form de cadastro no checkou

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -13,137 +13,163 @@
     <div class="row">
       <section class="end_estilo col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 teste2 primdiv">
       <p class="end_estilo row2">Endereço e Informações de Cobrança</p>
-      <form action="check.html" method="post">
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-3">
-      <label for="nome">Nome  <span class="asterisco"> * </span></label>   <br>
-      <input type="text"  class="col-12" id="nome">
-      <div class="invalid-feedback">
-        campo obrigatório
-      </div>
-     </div>
-    <div class="end_estilo col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-3">
-      <label for="nome">Sobrenome  <span class="asterisco"> * </span></label>   <br>
-      <input type="text"  class="col-12" id="sobrenome">
-      <div class="invalid-feedback">
-        campo obrigatório
-      </div>
-    </div>
+      @if (isset($errors) && count($errors) > 0)
+            <div class="alert alert-danger">
+              <strong>Xi, Budinha! Seu cadastro deu ruim</strong>
+              <ul>
+                @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+                @endforeach
+              </ul>
+            </div>
+            @endif
+          <form class="needs-validation" action="/acdb/public/cadastro" method="POST" novalidate>
+						{!! csrf_field()!!}
+						<div id="formcadastro" class="form-row">
+							<div class="form-group col-md-6">
+								<label for="registroNome">Nome</label>
+								<input type="text" class="form-control" id="registroNome" name="nome" placeholder="Nome" required>
+								<div class="invalid-feedback">
+									campo obrigatório
+							</div>
+						</div>
 
-      <div class="teste2 col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 mb-3 form-group">
-        <label for="cpfcnpj">CPF/CNPJ</label>
-          <select class="form-control" id="cpfcnpj" name="cpfcnpj">
-            <option selected name="pessoafisicaCliente" value="0">CPF</option>
-            <option name="pessoajuridicaCliente" value="1">CNPJ</option>
-          </select>
-          <div class="invalid-feedback">
-            campo obrigatório
-          </div>
-      </div>
+								<div class="form-group col-md-6">
+									<label for="registroSobrenome">Sobrenome</label>
+									<input type="text" class="form-control" id="registroSobrenome" name="sobrenome" placeholder="Sobrenome" required>
+									<div class="invalid-feedback">
+										campo obrigatório
+												</div>
+								</div>
 
-      <div class="teste2 col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 mb-3 form-group">
-         <label for="cpf_cnpj">CPF/CNPJ</label>
-         <input type="text" class="form-control" id="registroCpfCnpj" name="cpf_cnpj" placeholder="CPF/CNPJ (somente números)" required>
-            <div class="invalid-feedback">
-                       campo obrigatório
-              </div>
-      </div>
+									<div class="form-group col-md-3">
+										<label for="cpfcnpj">CPF/CNPJ</label>
+										<select class="form-control" id="cpfcnpj" name="cpfcnpj">
+										<option selected name="pessoafisicaCliente" value="0">CPF</option>
+										<option name="pessoajuridicaCliente" value="1">CNPJ</option>
+										</select>
+										<div class="invalid-feedback">
+											campo obrigatório
+										</div>
+									</div>
 
-
-    <div style="clear:both" class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-3">
-      <label for="endereco_cad">Endereço  <span class="asterisco"> * </span></label>   <br>
-      <input type="text"  class="col-12" id="endereco_cad">
-      <div class="invalid-feedback">
-        campo obrigatório
-      </div> </div>
-    <div class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-3">
-      <label for="complemento_end">Complemento(casa,apto) <span class="asterisco"> * </span></label>   <br>
-      <input type="text"  class="col-12" id="complemento_end">
-       <div class="invalid-feedback">
-        campo obrigatório
-      </div>
-    </div>
-    <div class="form-group col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 mb-3">
-      <label for="cep_end">CEP  <span class="asterisco"> * </span></label>   <br>
-      <input type="text"  class="col-12" id="cep_end">
-      <div class="invalid-feedback">
-        campo obrigatório
-      </div>
-     </div>
-
-    <div class="form-group teste2 col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 mb-3">
-      <label for="nome">Cidade  <span class="asterisco"> * </span> </label>   <br>
-      <input type="text" class="col-12">
-      <div class="invalid-feedback">
-        campo obrigatório
-      </div>
-      </div>
-
-    <div class="form-group teste2 col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 mb-3">
-      <label for="estado">Estado<span class="asterisco"> * </span></label>
-      <select id="estado" class="form-control" name="estado" required>
-        <option selected>Escolha...</option>
-        <option value="AC">Acre</option>
-        <option value="AL">Alagoas</option>
-        <option value="AP">Amapá</option>
-        <option value="AM">Amazonas</option>
-        <option value="BA">Bahia</option>
-        <option value="CE">Ceará</option>
-        <option value="DF">Distrito Federal</option>
-        <option value="ES">Espírito Santo</option>
-        <option value="GO">Goiás</option>
-        <option value="MA">Maranhão</option>
-        <option value="MT">Mato Grosso</option>
-        <option value="MS">Mato Grosso do Sul</option>
-        <option value="MG">Minas Gerais</option>
-        <option value="PA">Pará</option>
-        <option value="PB">Paraíba</option>
-        <option value="PR">Paraná</option>
-        <option value="PE">Pernambuco</option>
-        <option value="PI">Piauí</option>
-        <option value="RJ">Rio de Janeiro</option>
-        <option value="RN">Rio Grande do Norte</option>
-        <option value="RS">Rio Grande do Sul</option>
-        <option value="RO">Rondônia</option>
-        <option value="RR">Roraima</option>
-        <option value="SC">Santa Catarina</option>
-        <option value="SP">São Paulo</option>
-        <option value="SE">Sergipe</option>
-        <option value="TO">Tocantins</option>
-      </select>
-      <div class="invalid-feedback">
-        campo obrigatório
-      </div>
-      </div>
-
-    <div style="clear:both" class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-3">
-      <label for="nome">E-mail  <span class="asterisco"> * </span></label>   <br>
-      <input type="text"  class="col-12" id="email_end">
-      <div class="invalid-feedback">
-        campo obrigatório
-      </div>
-     </div>
-
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-3 form-group">
-        <label for="registroSenha">Senha</label>
-        <input type="password" class="form-control" id="registroSenha" name="senha" placeholder="Mínimo de 8 dígitos" required>
-      </div>
-      <div class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-3">
-        <label for="registroSenhaConf">Confirme sua Senha</label>
-        <input type="password" class="form-control required" id="registroSenhaConf" name="senha_confirmation" placeholder="Digite novamente sua senha" required>
-        @isset($msg_senha)
-        {{  $msg_senha  }}
-        @endisset
-        <div class="invalid-feedback">
-          campo obrigatório
-        </div>
-      </div>
+									<div class="form-group col-md-9">
+											<label for="cpf_cnpj">CPF/CNPJ</label>
+											<input type="text" class="form-control" id="registroCpfCnpj" name="cpf_cnpj" placeholder="CPF/CNPJ (somente números)" required>
+											<div class="invalid-feedback">
+												campo obrigatório
+														</div>
+										</div>
 
 
-    <div class="form-group col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-3 mt-3">
-    <input type="checkbox" name="aceitar" value="aceitar">
-     <label for="aceitar">Aceito os termos e condições de compra</label>
-     </div>
-   </form>
+
+							<div class="form-group col-md-6">
+								<label for="registroEmail">E-mail</label>
+								<input type="email" class="form-control" id="registroEmail" name="emailCliente" placeholder="E-mail" required>
+								<div class="invalid-feedback">
+									campo obrigatório
+								</div>
+							</div>
+							<div class="form-group col-md-6">
+								<label for="registroSenha">Senha</label>
+								<input type="password" class="form-control" id="registroSenha" name="senha" placeholder="Mínimo de 8 dígitos" required>
+							</div>
+							<div class="form-group col-md-6">
+								<label for="registroSenhaConf">Confirme sua Senha</label>
+								<input type="password" class="form-control required" id="registroSenhaConf" name="senha_confirmation" placeholder="Digite novamente sua senha" required>
+								@isset($msg_senha)
+								{{  $msg_senha  }}
+								@endisset
+								<div class="invalid-feedback">
+									campo obrigatório
+								</div>
+							</div>
+
+						<!-- </div> -->
+						<div class="form-group col-md-4">
+							<label for="data_nasc">Data de Nascimento</label>
+							<input type="date" class="form-control" id="data_nasc" name="data_nasc" maxlength="10" placeholder="DD/MM/AAAA" required>
+							<div class="invalid-feedback">
+								campo obrigatório
+							</div>
+						</div>
+
+						<div class="form-group col-md-2">
+							<label for="genero">Gênero</label>
+							<select class="form-control" id="genero" name="genero" required>
+								<option selected class="genmasc" id="genmasc" name="genmasc" value="M">M</option>
+								<option class="genmfem" id="genfem" name="genmfem" value="F">F</option>
+								<option class="genoutro" id="genoutro" name="genoutro" value="Outro">Outro</option>
+							</select>
+							<div class="invalid-feedback">
+								campo obrigatório
+							</div>
+						</div>
+
+						<div class="form-group col-md-6">
+							<label for="endereco">Endereço</label>
+							<input type="text" class="form-control" id="inputAddress" name="endereco" placeholder="Endereco">
+							<div class="invalid-feedback">
+								campo obrigatório
+							</div>
+						</div>
+
+						<div class="form-group col-md-6">
+							<label for="complemento">Complemento</label>
+							<input type="text" class="form-control" id="inputAddress2" name="complemento" placeholder="Complemento">
+							<div class="invalid-feedback">
+								campo obrigatório
+							</div>
+						</div>
+						</div>
+						<div class="form-row">
+
+							<div class="form-group col-md-5">
+								<label for="estado">Estado</label>
+								<select id="estado" class="form-control" name="estado" required>
+								</select>
+								<div class="invalid-feedback">
+									campo obrigatório
+								</div>
+							</div>
+
+							<div class="form-group col-md-5">
+								<label for="cidade">Cidade</label>
+								<select class="form-control" id="cidade" name="cidade" required>
+								</select>
+								<div class="invalid-feedback">
+									campo obrigatório
+								</div>
+							</div>
+
+							<div class="form-group col-md-2">
+								<label for="cep">Cep</label>
+								<input type="text" class="form-control" id="cep" name="cep" placeholder="CEP" required>
+								<div class="invalid-feedback">
+									campo obrigatório
+								</div>
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="form-check">
+								<input class="form-check-input" type="checkbox" id="checktermos" name="termos" required>
+								<label class="form-check-label" for="checktermos">
+									<div class="invalid-feedback">
+										campo obrigatório
+									</div>
+									Aceito os termos de uso
+								</label>
+							</div>
+						</div>
+						
+						<button type="submit" class="btn btn-primary">Cadastrar</button>
+				</form>
+				<div class="retorno">@isset($resultado)
+            <br>
+            <div class="alert alert-primary" role="alert">
+            {{$resultado}}
+            </div>
+        @endisset</div>
     </section>
 
 
@@ -233,5 +259,76 @@
        @endisset</div>
    </div>
  </div>
+ <script>
+	var request = new XMLHttpRequest();
+	request.open('GET', 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/' , true);
 
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400) {
+		// Success!
+		var data = JSON.parse(request.responseText);
+		let select = document.getElementById('estado');
+		let teste;
+		for(let estado of data){
+			// console.log(estado);
+		teste += "<option id='"+estado.nome+"'value='"+estado.id+"'>"+estado.nome+"</option>";
+		select.innerHTML = teste;
+		}} else {
+		// We reached our target server, but it returned an error
+		alert('error');
+		}};
+
+	request.onerror = function() {
+	// There was a connection error of some sort
+	};
+
+	request.send();
+
+window.onload = function() {
+
+	var estado = document.getElementById('estado');
+	var cidade = document.getElementById('cidade');
+
+	estado.onchange = function(){
+		var request = new XMLHttpRequest();
+		request.open('GET', 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/'+estado.selectedOptions[0].value+'/municipios', true);
+		request.onload = function() {
+			if (request.status >= 200 && request.status < 400) {
+			// Success!
+			var data = JSON.parse(request.responseText);
+			let teste;
+			// console.log(estado.selectedOptions)
+			for(let estado of data){
+				// console.log(estado.nome);
+			teste += "<option value='"+estado.nome+"'>"+estado.nome+"</option>";
+			cidade.innerHTML = teste;
+			}} else {
+			// We reached our target server, but it returned an error
+			alert('error');
+			}};
+
+			request.onerror = function() {
+			// There was a connection error of some sort
+			};
+
+			request.send();
+	}
+
+	//Máscara da data de Nascimento
+	var datanasc = document.getElementById('data_nasc');
+	console.log(datanasc);
+
+				datanasc.onkeyup = function mascaraData(){
+						if (datanasc.value.length == 2){
+							datanasc.value = datanasc.value + '/';
+
+						return true;
+						}
+						if (datanasc.value.length == 5){
+						datanasc.value = datanasc.value + '/';
+						return true;
+						}
+					}
+				}
+</script>
   	    @endsection
